11322a07773a82c17209a3f71370b2d0785d24ca
Jenny <coxj12@gmail.com>
2013-08-20 12:25:50 -0400

    First commit, includes X and O JPGs, index, game and design. Title and board appear, X appears onclick. Started work on playing an O response, works intermittently.

---
+++ game.js
@@ -0,0 +1,54 @@
+/**
+ * @author Jennifer Jensen
+ */
+
+function setImageVisible(id, visible, image) 
+{
+var img = document.getElementById(id+image);
+img.style.visibility = (visible ? 'visible' : 'hidden');
+}
+
+function getImageVisible(id, image)
+{
+	var img = document.getElementById(id+image);
+	return(img.style.visibility);
+}
+
+function cellAvailable(cell)
+{
+	if (getImageVisible(cell.id, "X") != "visible" && getImageVisible(cell.id, "O") != "visible")
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+function centerSquare()
+{
+	if (cellAvailable("cell5") != "visible" && getImageVisible("cell5", "X") != "visible")
+	{
+		setImageVisible("cell5", true, "O");
+	}
+}
+
+
+function play(cell) 
+{
+	//alert(cell.id);
+	if (cellAvailable(cell) != true)
+	{
+		alert("That spot is taken, please choose again");
+	}
+	else if (cellAvailable(cell) === true)
+	{
+		setImageVisible(cell.id, "visible", "X");
+		centerSquare();
+	}
+	
+
+	//TODO AI
+	//TODO game end, red C appears
+}
